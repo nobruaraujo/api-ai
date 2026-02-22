f867cc4f1a6e15a97c51dbd61a24cf3cd05493a1
Bruno Araujo <brunosousa357@gmail.com>
2026-02-22 16:44:40 -0300

    refactor: Remoção de classe não mais usada

--- src/main/java/com/nobru/api_ai/api/exception/BarberAvailabilityIllegalArgumentException.java
+++
@@ -1,9 +0,0 @@
-package com.nobru.api_ai.api.exception;
-
-import org.springframework.http.HttpStatus;
-
-public class BarberAvailabilityIllegalArgumentException extends DomainException {
-    public BarberAvailabilityIllegalArgumentException(String message) {
-        super(message, HttpStatus.BAD_REQUEST);
-    }
-}
